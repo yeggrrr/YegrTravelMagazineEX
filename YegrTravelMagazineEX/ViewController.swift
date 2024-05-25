fa9124539342e69ea52ae4637c249001421d235a
yeggrrr <yjkim981103@gmail.com>
2024-05-25 15:25:23 +0900

    Initial Commit

---
+++ YegrTravelMagazineEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YegrTravelMagazineEX
+//
+//  Created by YJ on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
