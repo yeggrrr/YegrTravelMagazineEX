758a771f0c47b567e182285582a6d2481f2bd60d
yeggrrr <yjkim981103@gmail.com>
2024-05-29 20:18:38 +0900

    [Feat] Segment 기능 추가 (전체, 국내, 해외)

--- YegrTravelMagazineEX/Cell/CityTableViewCell.swift
+++ YegrTravelMagazineEX/Cell/CityTableViewCell.swift
@@ -6,18 +6,47 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CityTableViewCell: UITableViewCell {
-
+    static let identifier = "CityTableViewCell"
+    
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var cityBackgroundView: UIView!
+    @IBOutlet var travelDestinationNameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configureUI()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureUI() {
+        mainImageView.contentMode = .scaleAspectFill
+        
+        mainImageView.layer.cornerRadius = 15
+        mainImageView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner]
+        mainImageView.layer.masksToBounds = true
+        
+        cityBackgroundView.layer.cornerRadius = 15
+        cityBackgroundView.layer.maskedCorners = .layerMaxXMaxYCorner
+        cityBackgroundView.layer.masksToBounds = true
+        
+        cityNameLabel.textColor = .white
+        cityNameLabel.textAlignment = .right
+        cityNameLabel.font = .boldSystemFont(ofSize: 17)
+        
+        travelDestinationNameLabel.textColor = .white
+        travelDestinationNameLabel.textAlignment = .left
+        travelDestinationNameLabel.font = .systemFont(ofSize: 14)
     }
     
+    func configureCell(data: City) {
+        let image = data.city_image
+        guard let url = URL(string: image) else { return }
+        mainImageView.kf.setImage(with: url)
+        cityNameLabel.text = "\(data.city_name) | \(data.city_english_name)"
+        travelDestinationNameLabel.text = data.city_explain
+    }
 }
--- YegrTravelMagazineEX/Controller/CityViewController.swift
+++ YegrTravelMagazineEX/Controller/CityViewController.swift
@@ -8,22 +8,75 @@
 import UIKit
 
 class CityViewController: UIViewController {
-
+    @IBOutlet var citySegmentedControl: UISegmentedControl!
+    @IBOutlet var cityTableView: UITableView!
+    
+    let data = CityInfo.city
+    var filteredDataList: [City] = []
+    var isFiltered = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureUI()
+        setSegmentControll()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureUI() {
+        // navigation title
+        navigationItem.title = "인기 도시"
+        
+        // tableview
+        cityTableView.delegate = self
+        cityTableView.dataSource = self
+        
+        let cityXib = UINib(nibName: CityTableViewCell.identifier, bundle: nil)
+        cityTableView.register(cityXib, forCellReuseIdentifier: CityTableViewCell.identifier)
     }
-    */
+    
+    func setSegmentControll() {
+        citySegmentedControl.addTarget(self, action: #selector(segmentValueChanged), for: .valueChanged)
+    }
+    
+    @objc func segmentValueChanged(segment: UISegmentedControl) {
+        isFiltered = true
+        
+        switch segment.selectedSegmentIndex {
+        case 0:
+            filteredDataList = CityInfo.city
+            print("0클릭됨")
+        case 1:
+            filteredDataList = CityInfo.city.filter{ $0.domestic_travel == true }
+            print("1클릭됨")
+        case 2:
+            filteredDataList = CityInfo.city.filter{ $0.domestic_travel == false }
+            print("2클릭됨")
+        default:
+            break
+        }
+        
+        cityTableView.reloadData()
+    }
+}
 
+extension CityViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltered {
+            return filteredDataList.count
+        } else {
+            return data.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CityTableViewCell.identifier, for: indexPath) as? CityTableViewCell else { return UITableViewCell() }
+        let data = isFiltered ? filteredDataList[indexPath.row] : data[indexPath.row]
+        cell.configureCell(data: data)
+        return cell
+    }
 }
