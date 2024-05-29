96ef3e25bf460e01da6f82a73d6d0662956afacd
yeggrrr <yjkim981103@gmail.com>
2024-05-29 19:16:12 +0900

    [Feat] CityVC UI & 데이터 불러오기

---
+++ YegrTravelMagazineEX/Cell/CityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CityTableViewCell.swift
+//  YegrTravelMagazineEX
+//
+//  Created by YJ on 5/29/24.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ YegrTravelMagazineEX/Model/City.swift
@@ -0,0 +1,8 @@
+//
+//  City.swift
+//  YegrTravelMagazineEX
+//
+//  Created by YJ on 5/29/24.
+//
+
+import Foundation
