80e4baef38e045173ac1921f209b452e7233a0bf
yeggrrr <yjkim981103@gmail.com>
2024-06-02 17:08:19 +0900

    [Add & Fix] ChatRoom Xib Cell  파일 생성 / left navigationBarItem UI 수정

---
+++ YegrTravelMagazineEX/Chat/View/ChatPartnerTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ChatPartnerTableViewCell.swift
+//  YegrTravelMagazineEX
+//
+//  Created by YJ on 6/2/24.
+//
+
+import UIKit
+
+class ChatPartnerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+ 
+    }
+
+ 
+    
+}
---
+++ YegrTravelMagazineEX/Chat/View/ChatUserTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUserTableViewCell.swift
+//  YegrTravelMagazineEX
+//
+//  Created by YJ on 6/2/24.
+//
+
+import UIKit
+
+class ChatUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+     
+    }
+    
+}
